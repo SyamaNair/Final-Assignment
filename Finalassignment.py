23e759f2a05d26ed2de21b460d183760d1120d51
SyamaNair <32523478+SyamaNair@users.noreply.github.com>
2022-10-07 15:47:17 -0700

    Add files via upload

---
+++ Finalassignment.py
@@ -0,0 +1,115 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "ec09714d-a3d8-4a5a-abad-7cc8e2fb5fa7",
+   "metadata": {},
+   "source": [
+    "# My Jupyter Notebook on IBM Watson Studio"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "765c2cb5-407b-47a5-ba45-5a2d1a2947a0",
+   "metadata": {},
+   "source": [
+    "**Syama Sasikumar**"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "bf2a0c9e-6706-4663-bef8-5da441fa8e36",
+   "metadata": {},
+   "source": [
+    "Data Engineer"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "af8650e0-4a21-4e47-9792-cbdf53943caa",
+   "metadata": {},
+   "source": [
+    "*I am interested in data science because I find Data studies really interesting and believe will add to my skill set*"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "77920458-a965-44d6-8c28-a9fbcce41459",
+   "metadata": {},
+   "source": [
+    "### Below code will give the sum of two numbers"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "b2de6754-3582-4e87-9a03-8bdec29949f1",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "sum: 100\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = int(50)\n",
+    "b = int(50)\n",
+    "result = a+b\n",
+    "print(\"sum:\", result)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b87d57a9-1356-4692-aa81-5fccb138feb9",
+   "metadata": {},
+   "source": [
+    "1. Numbered list item 1\n",
+    "1. Numbered list item 2\n",
+    "\n",
+    " - Bullet list item 1\n",
+    " - Bullet list item 2\n",
+    "\n",
+    " |Header1|Header2|Header3|\n",
+    " |-------|-------|-------|\n",
+    " |CellA1|CellA2|CellA3|\n",
+    " |CellB1|CellB2|CellB3|\n",
+    " |CellC1|CellC2|CellC3|\n",
+    " \n",
+    " https://www.google.com/\n",
+    " "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "00d9a36b-a984-4f7a-8c96-b2f9029c4c2c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python",
+   "language": "python",
+   "name": "conda-env-python-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
